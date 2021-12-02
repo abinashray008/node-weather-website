4797eb4b67c5ecf5620e6643b8c6264127347ea3
Abinash Ray <ray.abinash@gmail.com>
2021-12-02 11:49:07 -0700

    error handling for invalid addresses

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,11 +1,11 @@
 const request= require('request')
 const geoCode=(address,callback)=>{
-    const url='https://api.mapbox.com/geocoding/v5/mapbox.places/' +address+'.json?access_token=pk.eyJ1IjoiYWJpbmFzaHJheTAwNyIsImEiOiJja3dtZXI0ZmUyYnF6Mm5uc3l6YzB6MTN5In0.qeVBPiXqiYAjzbearpdq5Q'
+    const url='https://api.mapbox.com/geocoding/v5/mapbox.places/' +encodeURIComponent(address)+'.json?access_token=pk.eyJ1IjoiYWJpbmFzaHJheTAwNyIsImEiOiJja3dtZXI0ZmUyYnF6Mm5uc3l6YzB6MTN5In0.qeVBPiXqiYAjzbearpdq5Q'
     request({url:url,json: true},(error,response)=>{
         if(error){
             callback('Unable to connect to the API')
         }
-        else if(response.body.features.length===0){
+        else if(response.body.message==='Forbidden' || response.body.features.length===0){
             callback('Unable to find location')
         }else{
             callback(undefined,{
